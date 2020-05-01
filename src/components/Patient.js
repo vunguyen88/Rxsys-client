22fcd47b04ca65e0e231365d038e8581586f8189
unknown <nguyenanhhoanvu@gmail.com>
2020-05-01 02:47:34 -0400

    update some styles

--- src/components/Patient.js
+++ src/components/Patient.js
@@ -3,10 +3,15 @@ import withStyles from '@material-ui/core/styles/withStyles';
 import Typography from '@material-ui/core/Typography';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
+import Link from 'react-router-dom/Link';
 
 const styles = {
     card: {
-        display: 'flex'
+        display: 'flex',
+        marginBottom: 20,
+    },
+    content: {
+        padding: 25
     }
 }
 
@@ -14,9 +19,9 @@ class Patient extends Component {
     render() {
         const { classes, patient : {name, address_line, cell_phone, city, country, postal_code, patientID, sex, createdBy, createdOn} } = this.props
         return (
-            <Card>
-                <CardContent>
-                    <Typography variant="h5" component="h2">
+            <Card className={classes.card}>
+                <CardContent class={classes.content}>
+                    <Typography variant="h5" component="h2" component={Link} to={`/pharmacies/${name}`} color="primary">
                         {name}
                     </Typography>
                     <Typography className={classes.pos} color="textSecondary">
