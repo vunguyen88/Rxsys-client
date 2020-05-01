bb59019bbca436c7d660337cfba9bf0425243b16
unknown <nguyenanhhoanvu@gmail.com>
2020-05-01 00:41:08 -0400

    add route for home, signup, login page

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,25 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import Link from 'react-router-dom/Link';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Button from '@material-ui/core/Button';
 
 class Navbar extends Component {
     render() {
         return (
-            <div>
-                <h1>Home page</h1>
-            </div>
+            <AppBar>
+                <Toolbar className="nav-container">
+                    <Button color="inherit" component={Link} to='./home'>
+                        Home
+                    </Button>
+                    <Button color="inherit" component={Link} to='./signup'>
+                        Signup
+                    </Button>
+                    <Button color="inherit" component={Link} to='./login'>
+                        Login
+                    </Button>
+                </Toolbar>
+            </AppBar>
         )
     }
 }
